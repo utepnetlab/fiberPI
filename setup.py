d9bea9b073d3cedfdc6bbf060c778228a24c4718
Christopher Mendoza <camendoza7@miners.utep.edu>
2019-06-10 11:55:53 -0600

    PyPi setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="FiberPI",
-    version="0.0.1",
+    version="0.0.3",
     author="Christopher Mendoza",
     author_email="camendoza7@miners.utep.edu",
     description="Package to detect contaminated fiber connectors",
